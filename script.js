074b5dec950aa2801221184440a4e0220cbb324d
sashapoliti <sas.politi@gmail.com>
2024-03-28 12:39:37 +0100

    with vue (fail)

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 import { posts } from "./data.js";
-import { Post } from "./Post.js";
+/* import { Post } from "./Post.js"; */
 
-const container = document.getElementById('container');
+/* const container = document.getElementById('container'); */
 
-posts.forEach((el) => {
+/* posts.forEach((el) => {
     const post = new Post(el, container)
     post.printPost();
-})
+}) */
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            posts: posts
+        }
+    },
+    methods : {
+        getInitials(name) {
+                const initials = name.split(' ').map(el => el.charAt(0)).join('');
+                return initials;      
+        },
+        toggleLike(index) {
+            if (this.posts[index].liked) {
+                this.posts[index].likes--;
+            } else {
+                this.posts[index].likes++;
+            }
+            this.posts[index].liked = !this.posts[index].liked;
+        }
+    },
+    computed : {
+        
+    },
+    mounted() {
+        console.log(this.posts);
+    }
+}).mount('#app')
